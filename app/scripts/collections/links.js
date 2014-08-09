60566ba0e400d2c22e084c0eb1b408132d464847
Jason Kim <active_red@hotmail.com>
2014-08-09 02:02:12 -0700

    close #44: Day 27: Links /search?query=searchWord

--- app/scripts/collections/links.js
+++ app/scripts/collections/links.js
@@ -10,7 +10,16 @@ define([
   var LinksCollection = Backbone.Collection.extend({
     model: Link,
 
-    url: 'http://localhost:3000/api/v0/links'
+    initialize: function(models, options) {
+      var query = options.query;
+
+      if (query) {
+        this.url = 'http://localhost:3000/api/v0/search/links_nav?query='
+                   + query;
+      } else {
+        this.url = 'http://localhost:3000/api/v0/links';
+      }
+    }
 
   });
 
--- app/scripts/views/page_search.js
+++ app/scripts/views/page_search.js
@@ -31,19 +31,25 @@ define([
     initialize: function (options) {
       var query = options.query;
 
-      this.collectionLinks = new LinksCollection();
+      this.collectionLinks = new LinksCollection([], {
+        query: query
+      });
       this.collectionTags = new TagsCollection([], {
         query: query
       });
 
       this.listenTo( this.collectionTags, 'reset add change remove', this.render, this );
+      this.listenTo( this.collectionLinks, 'reset add change remove', this.render, this );
+
       this.collectionTags.fetch();
+      this.collectionLinks.fetch();
 
       // this.listenTo(this.model, 'change', this.render);
     },
 
     render: function () {
       // this.$el.html(this.template(this.model.toJSON()));
+      // TODO: Make sure links are also converted to JSON
       this.$el.html(this.template(this.collectionTags.toJSON()));
     }
   });
