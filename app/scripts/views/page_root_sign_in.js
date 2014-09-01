8a2688db05f59482d8c2b625a402c2f4f1a8da7a
Jason Kim <active_red@hotmail.com>
2014-09-01 03:25:26 -0700

    open a popup

--- app/scripts/views/page_root_sign_in.js
+++ app/scripts/views/page_root_sign_in.js
@@ -27,7 +27,7 @@ define([
     el: '.v-sign-in',
 
     events: {
-      'submit .e-page-root-sign-in': 'onSubmit'
+      'click .e-sign-in': 'onSubmit'
     },
 
     initialize: function () {
@@ -42,87 +42,9 @@ define([
 
     onSubmit: function(e) {
       e.preventDefault();
-      this.disabledSubmit(true);
 
-      var self = this,
-          session,
-          options,
-          determineUsernameOrEmail;
-
-      determineUsernameOrEmail = function(username_email) {
-        if (username_email.indexOf('@') === -1) {
-          return 'username';
-        } else {
-          return 'email';
-        }
-      };
-
-      session = {
-        'username_email': this.$el.find('#input-signin-username-email').val(),
-        'password': this.$el.find('#input-signin-password').val(),
-        'signin_type': ''
-      };
-      session.signin_type = determineUsernameOrEmail(session.username_email);
-
-      options = {
-        success: function() {
-          self.hideErrors();
-          Backbone.history.navigate('#', true);
-        },
-        error: function() {
-          self.disabledSubmit(false);
-          self.removeCurrentErrors();
-          self.showErrors();
-        }
-      };
-
-      this.model = new Session();
-      this.model.save(session, options);
-    },
-
-    showErrors: function() {
-      var self = this,
-          model = this.model,
-          alertMessage,
-          renderAlertList;
-
-      alertMessage = function() {
-
-        // Remove hidden class
-        self.$('.e-page-root-sign-in .form-message').removeClass('hidden');
-      };
-
-      renderAlertList = function() {
-        var html = self.errorMessageTemplate(model.toJSON()),
-            selector = '.e-page-root-sign-in .form-message';
-
-        self.$(selector).append(html);
-      };
-
-      _.each(model.errors, function(error) {
-        var controlGroup = self.$('#input-signin-' + error.name).parent();
-        controlGroup.addClass('has-error');
-      }, self);
-
-      alertMessage();
-      renderAlertList();
-    },
-
-    hideErrors: function() {
-      this.$('.control-group').removeClass('has-error');
-    },
-
-    removeCurrentErrors: function() {
-      var selector = '.form-message ul';
-      this.$(selector).remove();
-    },
-
-    disabledSubmit: function(state) {
-      var selector = '.btn.btn-default.e-sign-in';
-      this.$(selector).toggleClass('disabled', state);
+      window.open('http://localhost:3000/v0/auth/sign_in?api_key=key&redirect_uri=auth/supurl', 'Sign in using Supurl acount', 'height=500,width=400');
     }
-
-
   });
 
   return PageRootSignInView;
